f1f32369252f3f64cf3c9438da712b4b31e74053
İlyas Bedir <ilyas.bedir@threadinmotion.com>
2025-04-05 01:49:13 +0300

    Refactor Tetris game structure and enhance tetromino functionality
    - Updated `Game` struct in `src/main.zig` to use floating-point types for board dimensions and added tile size calculations.
    - Introduced a new `drawTetrominoe` function to render tetrominoes on the board with specific colors.
    - Refactored tetromino type definitions in `src/tetrominoes.zig` to use a struct with an enum for shape types and added shape data for each tetromino.
    - Improved board initialization to include tile color setup.

--- src/main.zig
+++ src/main.zig
@@ -6,15 +6,25 @@ const tetro = @import("tetrominoes.zig");
 const Game = struct {
     const Self = @This();
     // Game properties here
-    const boardStartCoordinateX = 50;
-    const boardStartCoordinateY = 10;
-    const boardWidth = 400;
-    const boardHeight = 800;
-    const boardRows = 20;
-    const boardColumn = 10;
+    const boardStartCoordinateX: f32 = 50;
+    const boardStartCoordinateY: f32 = 10;
+    const boardWidth: f32 = 400;
+    const boardHeight: f32 = 800;
+    const boardRows: f32 = 20;
+    const boardColumn: f32 = 10;
+    const tileWidth = boardWidth / boardColumn;
+    const tileHeight = boardHeight / boardRows;
+
+    comptime {
+        if (tileWidth != tileHeight) {
+            @compileError("Tile width and tile height are not equal");
+        }
+    }
+
     board: [boardRows][boardColumn]u8 = undefined,
-    currentPieceType: tetro.TetrominoesType = tetro.TetrominoesType.NO_SHAPE,
-    nextPieceType: tetro.TetrominoesType = tetro.TetrominoesType.NO_SHAPE,
+    tileColor: [boardRows][boardColumn]rl.Color = undefined,
+    currentPieceType: tetro.Tetrominoes.ShapeType = tetro.Tetrominoes.ShapeType.NONE,
+    nextPieceType: tetro.Tetrominoes.ShapeType = tetro.Tetrominoes.ShapeType.NONE,
     score: u32 = 0,
     level: u8 = 0,
     gameOver: bool = false,
@@ -24,23 +34,22 @@ const Game = struct {
     pub fn initBoard(self: *Self) void {
         // Initialize entire board to zeros
         @memset(std.mem.asBytes(&self.board), 0);
+        @memset(std.mem.asBytes(&self.tileColor), 0);
     }
 
     pub fn drawBoardGrid(self: *Self) void {
         _ = self;
         // Draw a grid with 10 columns and 20 rows
         const cellSize: f32 = 40.0; // Size of each cell in pixels
-        // const offsetX: i32 = 100; // X position offset
-        // const offsetY: i32 = 50; // Y position offset
         //
         for (0..boardColumn + 1) |i| {
             rl.drawLineEx(
                 rl.Vector2{
-                    .x = @as(f32, @floatFromInt(boardStartCoordinateX)) + @as(f32, @floatFromInt(i)) * cellSize,
+                    .x = boardStartCoordinateX + (@as(f32, @floatFromInt(i)) * cellSize),
                     .y = boardStartCoordinateY,
                 },
                 rl.Vector2{
-                    .x = @as(f32, @floatFromInt(boardStartCoordinateX)) + @as(f32, @floatFromInt(i)) * cellSize,
+                    .x = boardStartCoordinateX + (@as(f32, @floatFromInt(i)) * cellSize),
                     .y = boardStartCoordinateY + boardHeight,
                 },
                 1.0,
@@ -51,15 +60,33 @@ const Game = struct {
             rl.drawLineEx(
                 rl.Vector2{
                     .x = boardStartCoordinateX,
-                    .y = @as(f32, @floatFromInt(boardStartCoordinateY)) + @as(f32, @floatFromInt(j)) * cellSize,
+                    .y = boardStartCoordinateY + (@as(f32, @floatFromInt(j)) * cellSize),
                 },
                 rl.Vector2{
                     .x = boardStartCoordinateX + boardWidth,
-                    .y = @as(f32, @floatFromInt(boardStartCoordinateY)) + @as(f32, @floatFromInt(j)) * cellSize,
+                    .y = boardStartCoordinateY + (@as(f32, @floatFromInt(j)) * cellSize),
                 },
                 1.0,
                 rl.Color.black,
             );
+        }
+    }
+
+    pub fn drawTetrominoe(self: *Self) void {
+        for (0..boardRows) |row| {
+            for (0..boardColumn) |column| {
+                if (0 < self.board[row][column]) {
+                    rl.drawRectangleRec(
+                        .{
+                            .x = boardStartCoordinateX + (tileWidth * @as(f32, @floatFromInt(row))),
+                            .y = boardStartCoordinateY + (tileWidth * @as(f32, @floatFromInt(column))),
+                            .width = tileWidth,
+                            .height = tileHeight,
+                        },
+                        self.tileColor[row][column],
+                    );
+                }
+            }
         }
     }
 };
@@ -112,6 +139,21 @@ pub fn main() anyerror!void {
 
         // draw grid for the debug
         tetrisGame.drawBoardGrid();
+        //
+        tetrisGame.board[1][1] = 1;
+        tetrisGame.tileColor[1][1] = rl.Color.green;
+        tetrisGame.board[2][2] = 1;
+        tetrisGame.tileColor[2][2] = rl.Color.red;
+        tetrisGame.board[3][3] = 1;
+        tetrisGame.tileColor[3][3] = rl.Color.purple;
+        tetrisGame.board[4][4] = 1;
+        tetrisGame.tileColor[4][4] = rl.Color.yellow;
+        tetrisGame.board[5][5] = 1;
+        tetrisGame.tileColor[5][5] = rl.Color.blue;
+        tetrisGame.board[6][6] = 1;
+        tetrisGame.tileColor[6][6] = rl.Color.magenta;
+        // draw tetrominoes
+        tetrisGame.drawTetrominoe();
 
         //rl.drawText("Congrats!", 190, 200, 20, rl.Color.gray);
         //----------------------------------------------------------------------------------
